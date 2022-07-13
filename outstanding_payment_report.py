ada9aafa107f1f60abac173d1f446c25b54ca881
Ysuper <ysuper0412@yahoo.com.tw>
2022-07-13 15:07:52 +0800

    test drop_duplicates

--- outstanding_payment_report.py
+++ outstanding_payment_report.py
@@ -20,8 +20,7 @@ if __name__ == "__main__":
     # merge_df = pd.merge(merge_df, dscma.df, left_on="收款業務員", right_on="登入者代號")
     merge_df = pd.merge(merge_df, acrtb.df, on=["結帳單別", "結帳單號"])
     merge_df.index += 1
-    my_filters = ("客戶代號 == 'CAS90014' and 確認碼 == 'Y' and 結案碼 =='N' and 結帳單別 in ['612', '615']")
-    # my_filters = ("確認碼 == 'Y' and 結案碼 =='N' and 結帳單別 in ['612', '615']")
+    my_filters = ("客戶代號 == 'CAS90014' and 確認碼 == 'Y' and 結案碼 =='N' and 結帳單別 in ['611', '612', '619']")
     merge_df = merge_df.query(my_filters)
 
     output_df = pd.DataFrame()
@@ -30,10 +29,11 @@ if __name__ == "__main__":
     output_df.insert(3, "結帳單號", merge_df['結帳單別'].str.cat(merge_df['結帳單號'], sep='-'))
     output_df.insert(7, "原幣應收帳款", merge_df['應收金額'] + merge_df['營業稅額'])
     output_df.insert(8, "原幣未收帳款", merge_df['本幣應收金額'] + merge_df['本幣營業稅額'] - merge_df['本幣已收金額'])
-    output_df.insert(len(output_df.columns), "憑證號碼",
-                     ['-'.join(i) for i in zip(merge_df['憑證單別'], merge_df['憑證單號'], merge_df['憑證序號'])])
+    output_df.insert(len(output_df.columns), "憑證號碼", ['-'.join(i) for i in zip(merge_df['憑證單別'], merge_df['憑證單號'])])
     output_df = COMMON.object_to_float(output_df, ["原幣應收帳款", "原幣未收帳款"], 2)
     output_df = COMMON.object_to_float(output_df, ["匯率"], 4)
+    output_df.drop_duplicates("憑證號碼")
+    output_df = output_df.reset_index(level=0, drop=True)
     # print(output_df)
     print(output_df.to_string(index=False))
 
